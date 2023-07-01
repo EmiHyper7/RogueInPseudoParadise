ca7cea4ebe7713aa27d4e059dbac9d67a632a6c0
Remi <137903455+EmiHyper7@users.noreply.github.com>
2023-07-01 15:35:15 +0000

    updates colors of tile graphics to be more in line with traditional roguelikes

--- tile_types.py
+++ tile_types.py
@@ -38,12 +38,12 @@ SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
 floor = new_tile(
     walkable=True,
     transparent=True,
-    dark=(ord("."),  (64, 64, 64), (0, 0, 0)),
-    light=(ord("."),  (128, 128, 128), (0, 0, 0)),
+    dark=(ord("."),  (50, 50, 50), (0, 0, 0)),
+    light=(ord("."),  (100, 100, 100), (0, 0, 0)),
 )
 wall = new_tile(
     walkable=False,
     transparent=False,
-    dark=(ord("#"), (128, 128, 128), (0, 0, 0)),
-    light=(ord("#"), (224, 224, 224), (0, 0, 0)),
+    dark=(ord("#"), (100, 100, 100), (0, 0, 0)),
+    light=(ord("#"), (200, 200, 200), (0, 0, 0)),
 )
